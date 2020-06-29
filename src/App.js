921e3c113ae47b82f4bd0a177260f5701592bc28
xin.hu <xin.hu@xinhus-MBP.fios-router.home>
2020-06-29 10:52:33 -0400

    added basic html components for the app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import DateTimeSelection from "./components/DateTimeSelection";
 
 function App() {
+  const [day, setDay] = useState(365);
+  const [hour, setHour] = useState(28);
+  const [minute, setMinute] = useState(20);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Change Event</button>
+      <DateTimeSelection />
+      <div className="count-down">
+        <div className="day">{day}</div>
+        <div className="hours">{hour}</div>
+        <div className="minutes">{minute}</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/DateTimeSelection/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DateTimeSelection = () => {
+  return (
+    <div className="container">
+      <form>
+        <input type="date" />
+        <input type="time" />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default DateTimeSelection;
