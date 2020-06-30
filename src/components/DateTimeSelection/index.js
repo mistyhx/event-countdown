1eb118767fcc5789e4974ab30a79550a1b23a1cb
xin.hu <xin.hu@xinhus-MBP.fios-router.home>
2020-06-30 09:04:01 -0400

    updated the dateime picker

--- src/components/DateTimeSelection/index.js
+++ src/components/DateTimeSelection/index.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DateTimeSelection = () => {
+  const [datetime, setDatetime] = useState("");
   return (
     <div className="container">
-      <form>
-        <input type="date" />
-        <input type="time" />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          alert(typeof datetime);
+        }}
+      >
+        <input
+          type="datetime-local"
+          name="datetime-local"
+          value={datetime}
+          onChange={e => setDatetime(e.target.value)}
+        />
         <input type="submit" />
       </form>
     </div>
